91e252f18b0bbcbe0f8bb068e912d6879fc20ede
Carlos Roberto <carlosrjhoe@gmail.com>
2022-03-14 21:35:33 -0300

    Melhorando a aplicação, RequestFactory

--- animais/tests/test_urls.py
+++ animais/tests/test_urls.py
@@ -1,12 +1,17 @@
 import imp
-from django.test import TestCase
+from urllib import request, response
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from animais.views import index
 
 class AnimaisURLSTesttCase(TestCase):
     
+    def setUp(self):
+        self.factory = RequestFactory()
+    
     def test_rota_url_utiliza_view_index(self):
         '''teste de a home da aplicação utiliza a função index da view'''
-        root = reverse('/')
-        self.assertEqual(root.func, index)
+        request = self.factory.get('/')
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
         
